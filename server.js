26a97386bc8c890f9451c7a5e773080a34d4fcd2
Samthegent <dimejilekside@gmail.com>
2026-06-28 10:31:00 +0100

    Configure environment variables using dotenv and modify environment example file

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`${process.env.APP_NAME} running on port ${PORT}`);
+});
